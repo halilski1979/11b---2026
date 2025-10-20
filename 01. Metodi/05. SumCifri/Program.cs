496119d34524fccb2488b84e831e4329944c0c31
Halil <dospatliyski@gmail.com>
2025-10-20 12:58:17 +0300

    efer

---
+++ 01. Metodi/05. SumCifri/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _05._SumCifri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum(int.Parse(Console.ReadLine()));
+        }
+
+        public static void Sum(int n)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                
+                sum += n % 10;
+                n = n / 10;
+                
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
