c567fb10a5290bcdb7b658dc055ddc1a916f3554
Halil <dospatliyski@gmail.com>
2025-11-17 12:43:45 +0200

    dwqdw

---
+++ 02. OOP/Zadachi/03. PersonValidation/Person.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._PersonValidation
+{
+    public class Person
+    {
+        private string firstname;
+        private string lastname;
+        private int age;
+        private double salary;
+
+        public Person(string firstname, string lastname, int age, double salary)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.Age = age;
+            this.Salary = salary; 
+        }
+
+        public string FirstName
+        {
+            get { return firstname; }
+            set 
+            { 
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("trqbwa da sa pone 3 bukvi");
+                }
+                firstname = value;
+            }
+        }
+        public string LastName
+        {
+            get { return lastname; }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("trqbwa da sa pone 3 bukvi");
+                }
+                lastname = value;
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set {
+                if (value < 0)
+                {
+                    throw new ArgumentException("ne trqbwa da e po malko ot 0");
+                }
+                age = value;
+            }
+        }
+        public double Salary
+        {
+            get { return salary; }
+            set 
+            {
+                if (value < 460)
+                {
+                    throw new ArgumentException("ne moje da e po malko ot 460");
+                }
+                salary = value;
+            }
+        }
+
+        public double IncreaseSalary(double bonus)
+        {
+            if (this.age > 30)
+            {
+                return salary += salary * (bonus / 100);
+            }
+            else
+            {
+                return salary += salary * ((bonus / 2) / 100);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} get {this.Salary:f2} leva";
+        }
+    }
+}
---
+++ 02. OOP/Zadachi/03. PersonValidation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace _03._PersonValidation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> list = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var cmd = Console.ReadLine().Split(' ').ToList();
+
+                string ime = cmd[0];
+                string familiq = cmd[1];
+                int godini = int.Parse(cmd[2]);
+                double zaplata = double.Parse(cmd[3]);
+
+                Person p = new Person(ime, familiq, godini, zaplata);
+                list.Add(p);
+            }
+
+            double percent = double.Parse(Console.ReadLine());
+            foreach (var item in list)
+            {
+                item.IncreaseSalary(percent);
+            }
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
