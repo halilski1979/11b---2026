c9969bb4b1f802e248fdfae3fbccfbe357d5b2c7
Halil <dospatliyski@gmail.com>
2026-03-19 09:46:01 +0200

    wqq

---
+++ 03. Strukturi ot danni/07. Strings/06.1 - z3 - TextFilter/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _06._1___z3___TextFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] bannedWords = Console.ReadLine().Split(", ");            
+            string text = Console.ReadLine();
+
+           
+            foreach (string word in bannedWords)
+            {              
+                string replacement = new string('*', word.Length);               
+                text = text.Replace(word, replacement);
+            }
+                       
+            Console.WriteLine(text);
+        }
+    }
+}
